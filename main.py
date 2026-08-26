911bccf38844bbcfa7071eccdc8f1ea13ac1571d
Aadarsh Singh <aadarshsingh6200@gmail.com>
2026-08-26 13:42:28 +0530

    cleaned up main.py

--- main.py
+++ main.py
@@ -1,58 +1,3 @@
-# # # import speech_recognition as sr
-# # # import webbrowser
-# # # import pyttsx3 
-
-# # # recognizer = sr.Recognizer()
-# # # engine = pyttsx3.init()
-
-# # # def speak(text):
-# # #     engine.say(text)
-# # #     engine.runAndWait()
-
-# # # def processcommand(c):
-# # #     if"open google" in c.lower():
-# # #         webbrowser.open("https://google.com")
-# # #     elif"open facebook" in c.lower():
-# # #         webbrowser.open("https://facebook.com")
-# # #     elif"open youtube" in c.lower():
-# # #         webbrowser.open("https://youtube.com")
-# # #     elif"open linkedin" in c.lower():
-# # #         webbrowser.open("https://linkedin.com")
-# # #     elif"open instagram" in c.lower():
-# # #         webbrowser.open("https://instagram.com")
-
-# # # if __name__ == "__main__":
-# # #     speak("Intializing Nova....")
-# # #     while True:
-# # #         #Listen for the wake word "nova"
-# # #         # Obtain audio from the microphone 
-# # #         r = sr.Recognizer()
-
-
-
-# # #         print("recognizing...")
-# # #         try:
-# # #             with sr.Microphone() as source:
-# # #                 print("Listening...")
-# # #                 audio = r.listen(source,timeout=2,phrase_time_limit=3)
-# # #             word = r.recognize_google(audio)
-# # #             if(word.lower() == "nova"):
-# # #                 speak("yeah")
-# # #                 #Listen for command
-# # #                 with sr.Microphone() as source:
-# # #                     print("Listening...")
-# # #                     audio = r.listen(source)
-# # #                     command = r.recognize_google(audio)
-
-
-# # #                     processcommand(command)
-
-
-# # #             # print(command)
-# # #         except Exception as e:
-# # #             print("Error; {0}".format(e))
-
-
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
@@ -60,7 +5,7 @@ import time
 import musicLibrary
 
 
-# ---------------- SPEAK ---------------- #
+# ------- SPEAK 
 
 def speak(text):
     print("jarvis:", text)
@@ -74,7 +19,7 @@ def speak(text):
     engine.stop()
 
 
-# ---------------- COMMANDS ---------------- #
+# ---- COMMANDS 
 
 def process_command(command):
 
@@ -103,8 +48,9 @@ def process_command(command):
     elif "exit" in command or "quit" in command:
         speak("Goodbye sir")
 
-    elif"open chatgpt" in command:
-        webbrowser.open("https://chatgpt.com")
+    elif"cricbuzz" in command:
+        speak("Opening cricbuzz")
+        webbrowser.open("https://www.cricbuzz.com/")
 
     # elif c.lower().startswith("play"):
     #     song = c.lower().split[1]
@@ -129,7 +75,7 @@ def process_command(command):
     return True
 
 
-# ---------------- MAIN ---------------- #
+# ----- MAIN 
 
 if __name__ == "__main__":
 
@@ -143,7 +89,7 @@ if __name__ == "__main__":
 
         try:
 
-            # ===== LISTEN FOR AURA ===== #
+            # === LISTEN FOR JARVIS
 
             with sr.Microphone() as source:
 
@@ -159,7 +105,7 @@ if __name__ == "__main__":
 
             print("You said:", word)
 
-            # ===== WAKE WORD ===== #
+            # == WAKE WORD
 
             if "jarvis" in word.lower():
 
@@ -168,7 +114,7 @@ if __name__ == "__main__":
                 # IMPORTANT
                 time.sleep(1)
 
-                # ===== LISTEN FOR COMMAND ===== #
+                # === LISTEN FOR COMMAND 
 
                 with sr.Microphone() as source:
 
